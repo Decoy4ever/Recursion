58d7d0183e0a10b417dbcbdc92f6a0aec9d299e3
David Koy <dzilla398@gmail.com>
2025-08-08 16:29:48 +0930

    Remove the `createArr()` that requires the recursiveFib(n) as calculation of large values of n resulted in error. Solved by finding the product of the  previous fib array last tow index and pushing the result on the array.

--- fibonacci_sequence.js
+++ fibonacci_sequence.js
@@ -37,36 +37,35 @@ class Fib{
 
     recursiveFib(n){
 
-        if(n <= 0) return 0
-        if(n === 1) return 1
-        return this.recursiveFib(n - 1) + this.recursiveFib(n - 2)
-     
-    }
-
-    createArr(n){
         let arrayFib = [0,1]
-
         if(n <= 0) return [0]
         if(n === 1) return arrayFib
+        if(n >= 2){
 
-        for(let i = 2; i <= n; i++){
-            let value = this.recursiveFib(i)
-            arrayFib.push(value)
+            // get previous iteration of fib seq array
+            let previousFib = this.recursiveFib(n - 1)
+
+            // pattern the product of the last two index create the next fib seq num for the array
+            let fib_one = previousFib[previousFib.length - 2]
+            let fib_zero = previousFib[previousFib.length - 1]
+            previousFib.push(fib_one + fib_zero)
+
+            return previousFib
+
         }
 
-        return arrayFib
     }
 
 }
 
 
 const calculateFib = new Fib()
-// console.log(calculateFib.iterationFib(4))
-console.log(calculateFib.createArr(0))
-console.log(calculateFib.createArr(1))
-console.log(calculateFib.createArr(3))
-console.log(calculateFib.createArr(5))
-console.log(calculateFib.createArr(8))
+console.log(calculateFib.recursiveFib(0))
+console.log(calculateFib.recursiveFib(1))
+console.log(calculateFib.recursiveFib(2))
+console.log(calculateFib.recursiveFib(3))
+console.log(calculateFib.iterationFib(4))
+console.log(calculateFib.recursiveFib(8))
 
 
 
